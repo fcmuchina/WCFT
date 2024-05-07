554963e05912ebfc5145240a042fd604eaba52c8
Francis Chege Muchina <isaacfrancis77@gmail.com>
2024-05-07 16:23:20 +0300

    write code to extract house ids and add the ids to parent excel data

--- file_processors/main.py
+++ file_processors/main.py
@@ -1,40 +1,83 @@
-from openpyxl import load_workbook, workbook
-
-
-class Dependencies:
-    def __init__(self, orgin_data_file):
-        self.orgin_data_file = orgin_data_file
-    load_workbook_func = load_workbook
-    create_workbook_func = workbook
+from openpyxl import load_workbook, Workbook
 
 
 
-class ExcelFileOps:
-    def __init__(self, dependencies):
-        self.load_workbook_func = dependencies.load_workbook_func
-        self.orgin_data_file = dependencies.orgin_data_file
-        self.create_workbook_func = dependencies.create_workbook_func
+def fetch_workbook(filepath):
+    wb = load_workbook(filepath)
+    return wb
+
+def fetch_worksheet(wb, sheet_name):
+    ws = wb[sheet_name]
+    return ws
+
+day_2_survey_data_file = '../support_files/day_2_house_ids.xlsx'
+day_3_survey_data_file = '../support_files/day_3_house_ids.xlsx'
+day_4_survey_data_file = '../support_files/day_4_house_ids.xlsx'
 
 
-    def load_origin_data_file(self):
-        workbook = self.load_workbook_func(self.orgin_data_file)
-        return workbook
+def get_house_id_and_index(filepath, sheet_name):
+    house_id_and_index = {}
+    workbook = fetch_workbook(filepath)
+    worksheet = fetch_worksheet(workbook, sheet_name)
+    for row in worksheet.iter_rows(min_row=2, max_row=worksheet.max_row, min_col=1, max_col=2, values_only=True):
+        house_id_and_index[row[1]] = row[0] 
 
-    def get_sheet_names(self, workbook):
-        return workbook.sheetnames
+    return house_id_and_index
 
-    def create_new_workbook(self):
-        return self.create_workbook_func()
+def get_parent_indexes(filepath, sheet_name):
+    indexes = []
+    workbook = fetch_workbook(filepath)
+    worksheet = fetch_worksheet(workbook, sheet_name)
+    for row in worksheet.iter_rows(min_row=2, max_row=worksheet.max_row, min_col=1, max_col=1, values_only=True):
+        indexes.append(row[0])
 
-    def create_worksheet(self, workbook, sheet_name):
-        return workbook.create_sheet(sheet_name)
+    return indexes
 
-    def save_workbook(self, workbook, file_name):
-        workbook.save(file_name)
+
+def merge_index_to_house_id(filepath):
+    house_id_and_index = get_house_id_and_index(filepath, 'main_survey')
+    parent_indexes = get_parent_indexes(filepath, 'cont_survey')
+    single_house_id_and_index = tuple()
+    house_id_and_index_list = []
+    for index in parent_indexes:
+        single_house_id_and_index = (index, str(house_id_and_index[index]).replace(" ", ""))
+        house_id_and_index_list.append(single_house_id_and_index)
+
+    return house_id_and_index_list
+
+
+def write_to_file(source_filepath, output_file):
+    house_id_and_index_list = merge_index_to_house_id(source_filepath)
+    wb = Workbook()
+    ws = wb.active
+    ws.append(['Index', 'House ID'])
+    for house_id_and_index in house_id_and_index_list:
+        ws.append(house_id_and_index)
+    
+    wb.save(f'{output_file}.xlsx')
+
+    print('File has been written successfully')
+    return True
+
+
+write_to_file(day_4_survey_data_file, 'day_4_ids')
+write_to_file(day_3_survey_data_file, 'day_3_ids')
+write_to_file(day_2_survey_data_file, 'day_2_ids')
+
 
 
     
 
+# print(merge_index_to_house_id(day_4_survey_data_file))
+# print(get_house_id_and_index('../support_files/day_2_house_ids.xlsx', 'main_survey'))
+# print(get_parent_indexes(day_4_survey_data_file, 'cont_survey'))
+
+
+
+   
     
 
-    
+
+
+
+
